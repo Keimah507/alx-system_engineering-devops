33ad03ba51cf8f5687ff62dfe84ad9c6eac98927
root <githinjibrian5@gmail.com>
2022-09-20 12:44:32 -0700

    Advance

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,20 +1,19 @@
-# installs nginx using puppet
+#installs package nginx
 package { 'nginx':
 ensure => installed,
 }
 
-file_line { 'aaaaa':
-ensure => 'present',
+file_line { 'append line to nginx default config':
 path   => '/etc/nginx/sites-available/default',
 after  => 'listen 80 default_server;',
 line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
 }
 
 file { 'var/www/html/index.html':
-content => 'Hello World!',
+content => 'Hello World',
 }
 
 service { 'nginx':
 ensure  => running,
-require => Package['nginx'],
+require => package['nginx'],
 }
